abebc15fbd70c2d4078b7fa4eb8d9111f246f3e9
jincio <j.incio@pitt.edu>
2020-03-20 12:52:45 -0400

    Fake_propagacion UPDATE

--- panel1.R
+++ panel1.R
@@ -58,15 +58,15 @@ f2=data %>%
 
 f4=dat2 %>%
   ggplot(aes(x = Dia)) +
-  ylim(0,max(dat2$Total_Pruebas)*1.1)+
+  ylim(0,max(dat2$Total_Pruebas)*1.2)+
   geom_bar(aes(y = pruebas_dia, fill = "Nuevos"), stat = "identity", alpha=.5)+
   geom_label(aes(Dia, pruebas_dia, label = pruebas_dia), vjust = 0.5)+
   geom_line(aes(y = Total_Pruebas, col = "Acumulados"), size=1) +
   geom_point(aes(y = Total_Pruebas), col = "#8B1C62") +
   geom_label(aes(Dia, Total_Pruebas, label = Total_Pruebas), vjust = -0.5,
              label.size = 0.07)+
-  labs(y = "Numero de pruebas reportadas", color = " Casos", fill = " ",
-       title = paste0("Numero de pruebas aplicadas"),
+  labs(y = "Numero de pruebas reportadas", color = " Pruebas", fill = " ",
+       title = paste0("Numero de pruebas reportadas"),
        caption = paste0("Actualizado al", format(as.Date(max(data$Dia)),"%d/%m"))) +
   scale_fill_manual(values = c("Nuevos" = "#43CD80")) +
   scale_color_manual(values = c("Acumulados" = "#8B1C62")) +
@@ -74,17 +74,16 @@ f4=dat2 %>%
   theme_minimal() +
   theme(legend.position="bottom")
 
-
 f5=dat2 %>%
   ggplot(aes(x = Dia)) +
-  ylim(0,max(dat2$pruebas_dia)*1.05)+
+  ylim(0,max(dat2$pruebas_dia)*1.2)+
   geom_bar(aes(y = pos_new, fill = "Casos Positivos"), stat = "identity", alpha=.5)+
   geom_label(aes(Dia, pos_new, label = pos_new), vjust = 0.5)+
   geom_line(aes(y = pruebas_dia, col = "Pruebas"), size=1) +
   geom_point(aes(y = pruebas_dia), col = "#8B1C62") +
   geom_label(aes(Dia, pruebas_dia, label = pruebas_dia), vjust = -0.5,
-             label.size = 0.07)+
-  labs(y = "Numero de pruebas reportadas", color = " Casos", fill = " ",
+             label.size = 0.03)+
+  labs(y = " ", color = " ", fill = " ",
        title = paste0("Pruebas y casos positivos por dia"),
        caption = paste0("Actualizado al", format(as.Date(max(data$Dia)),"%d/%m"))) +
   scale_fill_manual(values = c("Casos Positivos" = "#43CD80")) +
@@ -92,7 +91,25 @@ f5=dat2 %>%
   #scale_y_continuous(sec.axis = sec_axis(~ .)) +
   theme_minimal() +
   theme(legend.position="bottom")
-#plot_grid(f1,f3,f2,f4)
+
+
+f6=dat2 %>%
+  ggplot(aes(x = Dia)) +
+  ylim(0,max(dat2$Total_Pruebas)*1.3)+
+  geom_bar(aes(y = Positivos, fill = "Positivos Acumulados"), stat = "identity", alpha=.5)+
+  geom_label(aes(Dia, Positivos, label = Positivos), vjust = 0.5,label.size = 0.10)+
+  geom_line(aes(y = Total_Pruebas, col = "Pruebas Acumuladas"), size=1) +
+  geom_point(aes(y = Total_Pruebas), col = "#8B1C62") +
+  geom_label(aes(Dia, Total_Pruebas, label = Total_Pruebas), vjust = -0.3,
+             label.size = 0.03)+
+  labs(y = "", color = "", fill = " ", 
+       title = paste0("Tendencias acumuladas"),
+       caption = paste0("Actualizado al", format(as.Date(max(data$Dia)),"%d/%m"))) +
+  scale_fill_manual(values = c("Positivos Acumulados" = "#43CD80")) +
+  scale_color_manual(values = c("Pruebas Acumuladas" = "#8B1C62")) +
+  #scale_y_continuous() +
+  theme_minimal() +
+  theme(legend.position="bottom")
 
 panel=ggarrange(f1,f4,f2,f3,
           ncol=2,nrow=2,
@@ -110,4 +127,19 @@ annotate_figure(panel,
                 #fig.lab = "Figure 1", fig.lab.face = "bold"
 )
 
-ggsave("panel.pdf")
+panel2=ggarrange(f5,f6,
+                ncol=1,nrow=2,
+                #widths = c(0.5,0.5,0.5,0.5),
+                font.label = list(size = 10, color = "black", face =
+                                    "bold", family = NULL))
+
+annotate_figure(panel2,
+                top = text_grob("COVID19-PERÚ", color = "red", face = "bold", size = 14),
+                bottom = text_grob("Fuente: \n Minsa. Ver (https://jincio.github.io/COVID_19_PERU/Propagacion.html)", color = "blue",
+                                   hjust = 1, x = 1, face = "italic", size = 10),
+                #left = text_grob("Figure arranged using ggpubr", color = "green", rot = 90),
+                #right = "www.joseincio.com",
+                #fig.lab = "Figure 1", fig.lab.face = "bold"
+)
+
+
