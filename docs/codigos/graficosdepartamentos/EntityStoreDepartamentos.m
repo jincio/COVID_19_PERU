72848d01344eddd70b8cc0b3877ea4f6fd7ead3b
jincio <j.incio@pitt.edu>
2020-05-04 16:55:07 -0400

    new plot

---
+++ docs/codigos/graficosdepartamentos/EntityStoreDepartamentos.m
@@ -0,0 +1,237 @@
+EntityStore[
+ <|"Types" -> <|"Departamento" -> 
+     <|"Entities" -> <|"AMAZONAS" -> <|"Label" -> "Amazonas", 
+          "WLEntity" -> Entity["AdministrativeDivision", {"Amazonas", 
+             "Peru"}], "Population" -> Quantity[379384, "People"], 
+          "MacroRegion" -> {"ORIENTE", "SELVA"}|>, 
+        "ANCASH" -> <|"Label" -> "Ancash", "WLEntity" -> 
+           Entity["AdministrativeDivision", {"Ancash", "Peru"}], 
+          "Population" -> Quantity[1083519, "People"], "MacroRegion" -> 
+           {"NORTE", "COSTA"}|>, "APURIMAC" -> 
+         <|"Label" -> "Apur\[IAcute]mac", "WLEntity" -> 
+           Entity["AdministrativeDivision", {"Apurimac", "Peru"}], 
+          "Population" -> Quantity[405759, "People"], "MacroRegion" -> 
+           {"CENTRO", "SIERRA"}|>, "AREQUIPA" -> <|"Label" -> "Arequipa", 
+          "WLEntity" -> Entity["AdministrativeDivision", {"Arequipa", 
+             "Peru"}], "Population" -> Quantity[1382730, "People"], 
+          "MacroRegion" -> {"SUR", "COSTA"}|>, "AYACUCHO" -> 
+         <|"Label" -> "Ayacucho", "WLEntity" -> 
+           Entity["AdministrativeDivision", {"Ayacucho", "Peru"}], 
+          "Population" -> Quantity[616176, "People"], "MacroRegion" -> 
+           {"CENTRO", "SIERRA"}|>, "CAJAMARCA" -> <|"Label" -> "Cajamarca", 
+          "WLEntity" -> Entity["AdministrativeDivision", {"Cajamarca", 
+             "Peru"}], "Population" -> Quantity[1341012, "People"], 
+          "MacroRegion" -> {"NORTE", "SIERRA"}|>, "CALLAO" -> 
+         <|"Label" -> "Callao", "WLEntity" -> Entity[
+            "AdministrativeDivision", {"Callao", "Peru"}], 
+          "Population" -> Quantity[994494, "People"], "MacroRegion" -> 
+           {"LIMACALLAO", "COSTA"}|>, "CUSCO" -> <|"Label" -> "Cusco", 
+          "WLEntity" -> Entity["AdministrativeDivision", {"Cusco", "Peru"}], 
+          "Population" -> Quantity[1205527, "People"], "MacroRegion" -> 
+           {"SUR", "SIERRA"}|>, "HUANCAVELICA" -> 
+         <|"Label" -> "Huancavelica", "WLEntity" -> 
+           Entity["AdministrativeDivision", {"Huancavelica", "Peru"}], 
+          "Population" -> Quantity[347639, "People"], "MacroRegion" -> 
+           {"CENTRO", "SIERRA"}|>, "HUANUCO" -> 
+         <|"Label" -> "Hu\[AAcute]nuco", "WLEntity" -> 
+           Entity["AdministrativeDivision", {"Huanuco", "Peru"}], 
+          "Population" -> Quantity[721047, "People"], "MacroRegion" -> 
+           {"CENTRO", "SIERRA"}|>, "ICA" -> <|"Label" -> "Ica", 
+          "WLEntity" -> Entity["AdministrativeDivision", {"Ica", "Peru"}], 
+          "Population" -> Quantity[850765, "People"], "MacroRegion" -> 
+           {"SUR", "COSTA"}|>, "JUNIN" -> <|"Label" -> "Jun\[IAcute]n", 
+          "WLEntity" -> Entity["AdministrativeDivision", {"Junin", "Peru"}], 
+          "Population" -> Quantity[1246038, "People"], "MacroRegion" -> 
+           {"CENTRO", "SIERRA"}|>, "LA LIBERTAD" -> 
+         <|"Label" -> "La Libertad", "WLEntity" -> 
+           Entity["AdministrativeDivision", {"LaLibertad", "Peru"}], 
+          "Population" -> Quantity[1778080, "People"], "MacroRegion" -> 
+           {"NORTE", "COSTA"}|>, "LAMBAYEQUE" -> <|"Label" -> "Lambayeque", 
+          "WLEntity" -> Entity["AdministrativeDivision", {"Lambayeque", 
+             "Peru"}], "Population" -> Quantity[1197260, "People"], 
+          "MacroRegion" -> {"NORTE", "COSTA"}|>, 
+        "LIMA" -> <|"Label" -> "Lima", "WLEntity" -> 
+           Entity["AdministrativeDivision", {"Lima", "Peru"}], 
+          "Population" -> Quantity[9485405, "People"], "SpecialPolygon" -> 
+           Polygon[GeoPosition[{{-12.519861, -76.783752}, {-12.558193, 
+             -76.727081}, {-12.581249000000001, -76.706527}, {-12.593471, 
+             -76.70903000000001}, {-12.622083, -76.677635}, {-12.699584, 
+             -76.656524}, {-12.720693, -76.630973}, {-12.745417, -76.63903}, 
+             {-12.850971, -76.518753}, {-12.927083, -76.505416}, {-12.946804, 
+             -76.50653}, {-12.953748, -76.52069}, {-13.012081, -76.481803}, 
+             {-13.030692, -76.490974}, {-13.323604, -76.24736}, {-13.302043, 
+             -76.202949}, {-13.22264, -76.16156}, {-13.187585, -76.155075}, 
+             {-13.098881, -76.025558}, {-13.041488, -75.969161}, {-13.000225, 
+             -75.960983}, {-12.991015, -75.93827}, {-13.03638, -75.849967}, 
+             {-13.02945, -75.823715}, {-13.052078, -75.819404}, {-13.060181, 
+             -75.800811}, {-13.018087000000001, -75.764556}, {-13.038369, 
+             -75.72604300000002}, {-13.03528, -75.69194}, 
+             {-13.014795000000001, -75.665534}, {-12.994292, -75.667442}, 
+             {-12.987685, -75.637359}, {-12.973498, -75.63813}, {-12.964738, 
+             -75.614021}, {-12.915041, -75.636901}, {-12.85937, -75.613189}, 
+             {-12.841733, -75.567474}, {-12.81744, -75.561164}, {-12.822484, 
+             -75.531631}, {-12.788873, -75.513839}, {-12.774977, -75.508155}, 
+             {-12.715926, -75.577079}, {-12.703532000000001, -75.554191}, 
+             {-12.683734, -75.551361}, {-12.641339000000002, -75.579589}, 
+             {-12.562837, -75.571914}, {-12.519058, -75.535835}, {-12.455199, 
+             -75.558059}, {-12.411129, -75.559944}, {-12.365214, -75.549003}, 
+             {-12.332504, -75.567588}, {-12.324739, -75.589164}, {-12.299144, 
+             -75.58699}, {-12.193581, -75.665451}, {-12.141419, -75.628913}, 
+             {-12.114502, -75.635185}, {-12.08218, -75.681983}, {-12.058071, 
+             -75.67527700000001}, {-12.042552, -75.694282}, {-12.038407, 
+             -75.741828}, {-12.058994, -75.766532}, {-12.013429, -75.820259}, 
+             {-12.02479, -75.833946}, {-12.006661, -75.843566}, {-11.993114, 
+             -75.885444}, {-11.997783, -75.903625}, {-11.983548, -75.926307}, 
+             {-11.994492, -75.947563}, {-11.983287, -75.974121}, {-11.993227, 
+             -75.993759}, {-11.963871, -76.037513}, {-11.939544, -76.053512}, 
+             {-11.912672, -76.058174}, {-11.902018, -76.049041}, {-11.879321, 
+             -76.059127}, {-11.84958, -76.093452}, {-11.780344, -76.135337}, 
+             {-11.782796, -76.152687}, {-11.729928, -76.201393}, 
+             {-11.701519000000001, -76.208229}, {-11.687561, -76.194297}, 
+             {-11.659352, -76.203353}, {-11.643698, -76.188201}, {-11.623938, 
+             -76.198204}, {-11.612739, -76.179496}, {-11.587046, -76.185577}, 
+             {-11.588302, -76.199188}, {-11.553667, -76.228027}, {-11.568711, 
+             -76.237205}, {-11.567917, -76.265899}, {-11.596286, -76.297203}, 
+             {-11.566686, -76.30474}, {-11.543702, -76.328804}, {-11.541655, 
+             -76.347221}, {-11.520453, -76.349601}, {-11.522807, -76.358551}, 
+             {-11.500851000000003, -76.376602}, {-11.443663000000003, 
+             -76.377143}, {-11.444794, -76.393959}, {-11.413055000000002, 
+             -76.414001}, {-11.402847, -76.400665}, {-11.354207, -76.422821}, 
+             {-11.334295, -76.456962}, {-11.291192, -76.478103}, {-11.269513, 
+             -76.457748}, {-11.246468, -76.490653}, {-11.236813, -76.483139}, 
+             {-11.186546, -76.492691}, {-11.117963000000001, -76.519165}, 
+             {-11.108711, -76.495948}, {-11.089318, -76.491149}, {-11.034006, 
+             -76.532928}, {-11.016401, -76.576889}, {-10.986521, -76.58213}, 
+             {-10.964524, -76.564895}, {-10.845319, -76.588134}, {-10.815513, 
+             -76.601905}, {-10.811566, -76.620201}, {-10.785182, -76.632797}, 
+             {-10.769282, -76.625946}, {-10.678766, -76.670791}, {-10.646776, 
+             -76.656158}, {-10.622542, -76.676033}, {-10.602943, -76.675918}, 
+             {-10.590164, -76.703895}, {-10.540283, -76.71096}, {-10.508455, 
+             -76.730293}, {-10.484991, -76.726203}, {-10.4672, -76.739585}, 
+             {-10.463556, -76.772781}, {-10.439663, -76.786926}, {-10.43865, 
+             -76.800445}, {-10.41901, -76.799819}, {-10.392277, -76.806098}, 
+             {-10.392636, -76.832183}, {-10.363115, -76.841049}, {-10.375176, 
+             -76.865829}, {-10.318329, -76.875999}, {-10.304686, -76.893318}, 
+             {-10.269972, -76.905914}, {-10.278711, -76.93708}, {-10.304568, 
+             -76.966072}, {-10.283965, -77.00505}, {-10.315934000000002, 
+             -77.01277900000001}, {-10.336663, -77.039596}, {-10.338342, 
+             -77.07113600000001}, {-10.404997, -77.111846}, {-10.465368, 
+             -77.174697}, {-10.482418, -77.177795}, {-10.506943, -77.168037}, 
+             {-10.551575, -77.1912}, {-10.54514, -77.250755}, {-10.560491, 
+             -77.271278}, {-10.556319, -77.290992}, {-10.608338, -77.344741}, 
+             {-10.637926, -77.366165}, {-10.638908, -77.379913}, {-10.680109, 
+             -77.40303}, {-10.708413, -77.384201}, {-10.730133, -77.414138}, 
+             {-10.723839, -77.436477}, {-10.744956, -77.485771}, {-10.73455, 
+             -77.524688}, {-10.788654, -77.592758}, {-10.766036, -77.610038}, 
+             {-10.75658, -77.588218}, {-10.72603, -77.576339}, {-10.664729, 
+             -77.573738}, {-10.644408, -77.585647}, {-10.587564, -77.57373}, 
+             {-10.584201, -77.592681}, {-10.601854, -77.637374}, {-10.522722, 
+             -77.646423}, {-10.539187, -77.670776}, {-10.536085, -77.690338}, 
+             {-10.568009, -77.737213}, {-10.5313, -77.745674}, {-10.49825, 
+             -77.706886}, {-10.431568, -77.68502}, {-10.406973, -77.626625}, 
+             {-10.383216, -77.599266}, {-10.363017000000001, -77.598701}, 
+             {-10.327378, -77.620147}, {-10.322796, -77.632781}, {-10.344362, 
+             -77.651535}, {-10.322029, -77.698371}, {-10.328006, -77.75624}, 
+             {-10.406682, -77.750572}, {-10.476837, -77.799095}, {-10.513944, 
+             -77.771751}, {-10.569603, -77.77069}, {-10.584521, -77.8031}, 
+             {-10.567794, -77.839973}, {-10.611527, -77.885971}, {-10.699304, 
+             -77.811805}, {-10.725694000000003, -77.805419}, {-10.765137, 
+             -77.761802}, {-10.772916, -77.765975}, {-10.791527, -77.745697}, 
+             {-10.819306, -77.751808}, {-10.852639, -77.708473}, {-10.88665, 
+             -77.703475}, {-10.949861, -77.661529}, {-10.998195, -77.649024}, 
+             {-11.013472, -77.662635}, {-11.05375, -77.637084}, {-11.093473, 
+             -77.635414}, {-11.116248, -77.612915}, {-11.127082, -77.62014}, 
+             {-11.176251, -77.594024}, {-11.214861, -77.60458300000002}, 
+             {-11.21097, -77.63291900000002}, {-11.220972, -77.649581}, 
+             {-11.273471, -77.642364}, {-11.290139, -77.657081}, {-11.304304, 
+             -77.647918}, {-11.449027, -77.382843}, {-11.517915, -77.302635}, 
+             {-11.569027, -77.273475}, {-11.582359, -77.27153}, {-11.587638, 
+             -77.28347}, {-11.645416, -77.212638}, {-11.67875, -77.194862}, 
+             {-11.694582, -77.199584}, {-11.743748, -77.169586}, {-11.773751, 
+             -77.17736}, {-11.767916, -77.19902800000001}, {-11.812638, 
+             -77.17736}, {-11.82986, -77.189132}, {-11.820111, -77.126029}, 
+             {-11.875446, -77.09365}, {-11.901045, -77.08995}, {-11.934946, 
+             -77.103034}, {-11.949664, -77.09539}, {-11.937935, -77.128768}, 
+             {-11.976293, -77.112876}, {-11.992721, -77.119232}, {-12.012475, 
+             -77.088294}, {-12.032565, -77.09539}, {-12.047543, -77.078254}, 
+             {-12.06165, -77.088073}, {-12.063433, -77.108184}, {-12.080366, 
+             -77.110092}, {-12.080563, -77.1099165}, {-12.08076, -77.109741}, 
+             {-12.13486, -77.030136}, {-12.154026, -77.024864}, 
+             {-12.166528000000001, -77.038475}, {-12.206805, -77.039306}, 
+             {-12.246248, -76.941253}, {-12.299304, -76.85624600000001}, 
+             {-12.315971, -76.836524}, {-12.343193, -76.827362}, {-12.339583, 
+             -76.819305}, {-12.352638, -76.817085}, {-12.36236, -76.798469}, 
+             {-12.404026, -76.77597}, {-12.439028, -76.775138}, {-12.444026, 
+             -76.786529}, {-12.478471, -76.791252}, {-12.472639, -76.802635}, 
+             {-12.495139, -76.805694}, {-12.509305, -76.804031}}]], 
+          "MacroRegion" -> {"LIMACALLAO", "COSTA"}|>, 
+        "LORETO" -> <|"Label" -> "Loreto", "WLEntity" -> 
+           Entity["AdministrativeDivision", {"Loreto", "Peru"}], 
+          "Population" -> Quantity[883510, "People"], "MacroRegion" -> 
+           {"ORIENTE", "SELVA"}|>, "MADRE DE DIOS" -> 
+         <|"Label" -> "Madre de Dios", "WLEntity" -> 
+           Entity["AdministrativeDivision", {"MadreDeDios", "Peru"}], 
+          "Population" -> Quantity[141070, "People"], "MacroRegion" -> 
+           {"ORIENTE", "SELVA"}|>, "MOQUEGUA" -> <|"Label" -> "Moquegua", 
+          "WLEntity" -> Entity["AdministrativeDivision", {"Moquegua", 
+             "Peru"}], "Population" -> Quantity[174863, "People"], 
+          "MacroRegion" -> {"SUR", "COSTA"}|>, "PASCO" -> 
+         <|"Label" -> "Pasco", "WLEntity" -> Entity["AdministrativeDivision", 
+            {"Pasco", "Peru"}], "Population" -> Quantity[254065, "People"], 
+          "MacroRegion" -> {"CENTRO", "SIERRA"}|>, 
+        "PIURA" -> <|"Label" -> "Piura", "WLEntity" -> 
+           Entity["AdministrativeDivision", {"Piura", "Peru"}], 
+          "Population" -> Quantity[1856809, "People"], "MacroRegion" -> 
+           {"NORTE", "COSTA"}|>, "PUNO" -> <|"Label" -> "Puno", 
+          "WLEntity" -> Entity["AdministrativeDivision", {"Puno", "Peru"}], 
+          "Population" -> Quantity[1172697, "People"], "MacroRegion" -> 
+           {"SUR", "SIERRA"}|>, "SAN MARTIN" -> 
+         <|"Label" -> "San Mart\[IAcute]n", "WLEntity" -> 
+           Entity["AdministrativeDivision", {"SanMartin", "Peru"}], 
+          "Population" -> Quantity[813381, "People"], "MacroRegion" -> 
+           {"ORIENTE", "SELVA"}|>, "TACNA" -> <|"Label" -> "Tacna", 
+          "WLEntity" -> Entity["AdministrativeDivision", {"Tacna", "Peru"}], 
+          "Population" -> Quantity[329332, "People"], "MacroRegion" -> 
+           {"SUR", "COSTA"}|>, "TUMBES" -> <|"Label" -> "Tumbes", 
+          "WLEntity" -> Entity["AdministrativeDivision", {"Tumbes", "Peru"}], 
+          "Population" -> Quantity[224863, "People"], "MacroRegion" -> 
+           {"NORTE", "COSTA"}|>, "UCAYALI" -> <|"Label" -> "Ucayali", 
+          "WLEntity" -> Entity["AdministrativeDivision", {"Ucayali", 
+             "Peru"}], "Population" -> Quantity[496459, "People"], 
+          "MacroRegion" -> {"ORIENTE", "SELVA"}|>|>, 
+      "Properties" -> <|"Population" -> <|"Label" -> "Poblaci\[OAcute]n"|>, 
+        "Polygon" -> <|"Label" -> "Pol\[IAcute]gono", "DefaultFunction" -> 
+           Function[e, Replace[e["SpecialPolygon"], _?MissingQ :> 
+              e["WLEntity"]["Polygon"]]]|>, "Position" -> 
+         <|"Label" -> "Posici\[OAcute]n del centro", "DefaultFunction" -> 
+           Function[e, e["WLEntity"]["Position"]]|>, 
+        "Name" -> <|"Label" -> "Nombre", "DefaultFunction" -> 
+           Function[e, e["Label"]]|>, "MacroRegion" -> 
+         <|"Label" -> "Macrorregiones"|>, "Label" -> <||>, 
+        "WLEntity" -> <||>, "SpecialPolygon" -> <||>|>, 
+      "EntityClasses" -> <|"PERU" -> <|"Entities" -> {"AMAZONAS", "ANCASH", 
+            "APURIMAC", "AREQUIPA", "AYACUCHO", "CAJAMARCA", "CALLAO", 
+            "CUSCO", "HUANCAVELICA", "HUANUCO", "ICA", "JUNIN", 
+            "LA LIBERTAD", "LAMBAYEQUE", "LIMA", "LORETO", "MADRE DE DIOS", 
+            "MOQUEGUA", "PASCO", "PIURA", "PUNO", "SAN MARTIN", "TACNA", 
+            "TUMBES", "UCAYALI"}, "Label" -> "Per\[UAcute]"|>, 
+        "CENTRO" -> <|"Entities" -> {"APURIMAC", "AYACUCHO", "HUANCAVELICA", 
+            "HUANUCO", "JUNIN", "PASCO"}, "Label" -> 
+           "Macro Regi\[OAcute]n Centro"|>, "NORTE" -> 
+         <|"Entities" -> {"ANCASH", "CAJAMARCA", "LA LIBERTAD", "LAMBAYEQUE", 
+            "PIURA", "TUMBES"}, "Label" -> "Macro Regi\[OAcute]n Norte"|>, 
+        "SUR" -> <|"Entities" -> {"AREQUIPA", "CUSCO", "ICA", "MOQUEGUA", 
+            "PUNO", "TACNA"}, "Label" -> "Macro Regi\[OAcute]n Sur"|>, 
+        "ORIENTE" -> <|"Entities" -> {"AMAZONAS", "LORETO", "MADRE DE DIOS", 
+            "SAN MARTIN", "UCAYALI"}, "Label" -> 
+           "Macro Regi\[OAcute]n Oriente"|>, "LIMACALLAO" -> 
+         <|"Entities" -> {"CALLAO", "LIMA"}, "Label" -> "Lima y Callao"|>, 
+        "COSTA" -> <|"Entities" -> {"ANCASH", "AREQUIPA", "CALLAO", "ICA", 
+            "LA LIBERTAD", "LAMBAYEQUE", "LIMA", "MOQUEGUA", "PIURA", 
+            "TACNA", "TUMBES"}, "Label" -> "departamentos de la costa"|>, 
+        "SIERRA" -> <|"Entities" -> {"APURIMAC", "AYACUCHO", "CAJAMARCA", 
+            "CUSCO", "HUANCAVELICA", "HUANUCO", "JUNIN", "PASCO", "PUNO"}, 
+          "Label" -> "departamentos de la sierra"|>, 
+        "SELVA" -> <|"Entities" -> {"AMAZONAS", "LORETO", "MADRE DE DIOS", 
+            "SAN MARTIN", "UCAYALI"}, "Label" -> 
+           "departamentos de la selva"|>|>|>|>|>]
